40101807f80849fffbd60bf0de80c1ae574c73dd
ALEX DOS SANTOS DIAS <alex.dias@prefeitura.riobranco>
2024-10-15 11:01:54 -0500

    static data assets and controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo 'Hello Word';
 });
+
+Route::get('/showData', [MainController::class, 'showData']);
