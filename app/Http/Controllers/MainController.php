aa43c4cd5313e5adf2ded7415777efcdfa0c5a7b
ALEX DOS SANTOS DIAS <alex.dias@prefeitura.riobranco>
2024-10-16 09:27:22 -0500

    preparing the game - part 1

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller{
@@ -15,5 +16,36 @@ class MainController extends Controller{
 
     public function showData(){
         return response()->json($this->app_data);
+    }
+
+    
+    public function startGame(): View{
+        return view('home');
+    }
+
+    public function prepareGame(Request $request){
+        //validate request
+        $request->validate(
+            [
+                'total_questions' => 'required|integer|min:3|max:30'
+            ],
+            [
+                'total_questions.required' => 'O número de questões deve ser definido',
+                'total_questions.integer' => 'O número deve ser inteiro',
+                'total_questions.min' => 'O questionário de ter pelo menos :min questões',
+                'total_questions.min' => 'O questionário de ter no máximo :max questões',
+            ]
+        );
+        
+        // get total questions
+        $total_questions = intval($request->input('total_questions'));
+
+        // prepare all the quiz structure
+        $quiz = $this->prepareQuiz($total_questions);
+
+    }
+
+    private function prepareQuiz($total_questions){
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-
-Route::get('/showData', [MainController::class, 'showData']);
+// start game
+Route::get('/',  [MainController::class, 'startGame'])->name('start_game');
+Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_game');
