62674c089a41c7871c25fa68c4966b23bb38d52b
ALEX DOS SANTOS DIAS <alex.dias@prefeitura.riobranco>
2024-10-16 11:31:57 -0500

    preparing the game - part 2

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -43,9 +43,38 @@ class MainController extends Controller{
         // prepare all the quiz structure
         $quiz = $this->prepareQuiz($total_questions);
 
+        dd($quiz);
+
+        
     }
 
     private function prepareQuiz($total_questions){
+        $questions = [];
+        $total_countries = count($this->app_data);
 
+        //create countries index for unique questions
+        $indexes = range(0, $total_countries -1);
+        shuffle($indexes);
+        $indexes = array_slice($indexes, 0, $total_questions);
+
+        // create array of questions
+        $question_number = 1;
+        foreach($indexes as $index){
+            $question['question_number'] = $question_number++;
+            $question['country'] = $this->app_data[$index]['country'];
+            $question['correct_answer'] = $this->app_data[$index]['capital'];
+
+            // wrong answer
+            $other_capitals = array_column($this->app_data, 'capital');
+
+            // remove correct answers
+            $other_capitals = array_diff($other_capitals, [$question['correct_answer']]);
+
+            // shuffle the wrong answers
+            shuffle($other_capitals);
+            $question['wrong_answers'] = array_slice($other_capitals, 0, 3);
+        }
+
+        return $questions;
     }
 }
